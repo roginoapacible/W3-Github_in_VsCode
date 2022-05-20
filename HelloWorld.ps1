1106c857ef4eae98e7e611eeb6631f20b16b1e75
roginoa <roginoapacible@gmail.com>
2022-05-19 18:27:51 -0700

    added second script

--- HelloWorld.ps1
+++ HelloWorld.ps1
@@ -3,3 +3,10 @@ $name = $env:USERNAME
 Write-host "Hello world!!, Your name is $name, Today is $date"
 
 #This is a comment to my new powershell script
+
+Write-Host "Hello world"
+$date = Get-Date -Format MMMM/dd/yyyy
+$time = Get-date -format hh:mm:tt
+Write-Host "Today is $date, time is $time"
+Write-Host "Hello $env:COMPUTERNAME, This is your first PowerShell Core Script"
+Write-host "Added update to the repo"
